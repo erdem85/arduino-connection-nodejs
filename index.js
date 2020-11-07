a366633bce7b55c59bf7e83a1b2efd69de5f918b
erdem <erdemhso@mail.ru>
2020-11-07 17:14:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const
     five = require("johnny-five"),
-    Discord = require("discord.js")
+    Discord = require("discord.js"),
+    fetch = require("node-fetch")
+
 const
     board = new five.Board(),
     client = new Discord.Client()
@@ -9,13 +11,12 @@ board.on("ready", () => {
     const led = new five.Led(13);
     const lm35 = new five.Thermometer({controller: "LM35", pin: "A0"});
     const piyano = new five.Piezo(3);
-    client.on("message", msg => {
-        if(msg.content == "!ledopen") led.on(), msg.channel.send("`OK.`");
-        if(msg.content == "!ledclose") led.off(), msg.channel.send("`OK.`");
-        if(msg.content == "!roomtemp") msg.channel.send(lm35.celsius+" °C");
-        if(msg.content == "!playsong") msg.channel.send("`OK.`"), piyano.play({song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -", beats: 1 / 4, tempo: 50});
-    });
+    setInterval(function (){
+        try{
+            fetch(`http://localhost:8080/db?auth=authkey&mod=post&id=${lm35.celsius}`);
+        }catch(err){
+            console.log("temperature couldnt be send")
+            return led.on();
+        }
+    }, 5000);
 });
-
-client.on("ready", () => console.log(`${client.user.tag} joined.`));
-client.login("your bot token");

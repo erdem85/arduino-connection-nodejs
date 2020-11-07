910b62e427d8698a95a5ee5fc3d4406ce1a97f21
erdem <erdemhso@mail.ru>
2020-11-07 10:18:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const
+    five = require("johnny-five"),
+    Discord = require("discord.js")
+
+const
+    board = new five.Board(),
+    client = new Discord.Client()
+
+board.on("ready", () => {
+    const led = new five.Led(13);
+    const lm35 = new five.Thermometer({controller: "LM35", pin: "A0"});
+    const piyano = new five.Piezo(3);
+    client.on("message", msg => {
+        if(msg.content == "!led-aç") led.on(), msg.channel.send("`OK.`");
+        if(msg.content == "!led-kapat") led.off(), msg.channel.send("`OK.`");
+        if(msg.content == "!oda-sıcaklık") msg.channel.send(lm35.celsius+" °C");
+        if(msg.content == "!şarkı-çal") msg.channel.send("`OK.`"), piyano.play({song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -", beats: 1 / 4, tempo: 50});
+    });
+});
+
+client.on("ready", () => console.log(`${client.user.tag} joined.`));
+client.login("NzcwMjE1MTYxOTM1NjkxODA2.X5aUxA.qn79et7-8NHG3itHbYEaWbZ7RvQ");

0191e35a3f78223d115bd587697fb83b1be863de
erdem <erdemhso@mail.ru>
2020-11-07 10:20:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ board.on("ready", () => {
     const lm35 = new five.Thermometer({controller: "LM35", pin: "A0"});
     const piyano = new five.Piezo(3);
     client.on("message", msg => {
-        if(msg.content == "!led-aç") led.on(), msg.channel.send("`OK.`");
-        if(msg.content == "!led-kapat") led.off(), msg.channel.send("`OK.`");
-        if(msg.content == "!oda-sıcaklık") msg.channel.send(lm35.celsius+" °C");
-        if(msg.content == "!şarkı-çal") msg.channel.send("`OK.`"), piyano.play({song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -", beats: 1 / 4, tempo: 50});
+        if(msg.content == "!ledopen") led.on(), msg.channel.send("`OK.`");
+        if(msg.content == "!ledclose") led.off(), msg.channel.send("`OK.`");
+        if(msg.content == "!roomtemp") msg.channel.send(lm35.celsius+" °C");
+        if(msg.content == "!playsong") msg.channel.send("`OK.`"), piyano.play({song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -", beats: 1 / 4, tempo: 50});
     });
 });
 
